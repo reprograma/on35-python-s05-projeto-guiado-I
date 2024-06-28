278feca63cae569a9626df7cbe97da90f93ddd30
Daniele Junior <daniele.rodrigues@loft.com.br>
2024-06-28 14:36:24 -0300

    codigo

---
+++ projeto/main.py
@@ -0,0 +1,6 @@
+import print_reprograma as pr
+
+pr.limpar()
+pr.retangulo('{reprograma}\nProjeto Guiado 1\nTerminal de Vendas',sv=1,tamanho=100, margem=10,cor_texto='azul negrito',cor_barra='magenta')
+pr.separador(120,cor_texto='ciano')
+
