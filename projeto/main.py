89528bb80eb96c812ae62e2fd8e0a0aa5c5ee76a
Jenifer <jenifersilva429@gmail.com>
2024-07-04 14:39:54 -0300

    projeto guiado

--- projeto/main.py
+++ projeto/main.py
@@ -1,3 +1,32 @@
 import print_reprograma as pr
+def imprimir_ajuda():
+    pr.imprimir('Ajuda')
 
-pr.imprimir('Olá mundo')
+
+opcao = ''
+erro = ''
+tela = ''
+while ((opcao != 's') and (opcao != 'sair')):
+    pr.limpar
+    pr.retangulo('{Reprograma}\n Projeto Guiado I\nTerminal de vendas', tamanho=120, cor_barra='magenta',cor_texto='azul')
+    pr.separador(120, cor_texto= 'azul')
+    #erro 
+    if(erro != ''):
+        pr.imprimir(erro,cor_texto='vermelho negrito', tamanho=120, alinhar='centro')
+        pr.separador(120,cor_texto='azul')
+
+        #tela ajuda 
+    if (tela == ''):
+        pr.imprimir('tela inicial')
+    
+    pr.imprimir('[S]Sair', '[H]Ajuda',tamanho=120,alinhar='fim', caracter= '═', end='┤')
+
+    #o end pula uma linha. 
+
+    opcao = input().lower()
+    if((opcao != 's') or (opcao != 'sair')):
+        break
+    elif (opcao == 'h'):
+        tela = 'ajuda'
+    else: 
+        erro = 'A opçaõ não existe!!!'
